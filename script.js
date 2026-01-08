438b541adcebb9a6122b7d4d1c7b0995cea26a80
musheghwork <mushegh.work@gmail.com>
2026-01-08 15:56:25 +0400

    fix footer links

--- script.js
+++ script.js
@@ -191,3 +191,8 @@ function toggleCont() {
     el.style.display = el.style.display === "block" ? "none" : "block";
   });
 }
+
+const yearSpan = document.querySelector('#year');
+if (yearSpan) {
+    yearSpan.innerText = new Date().getFullYear();
+}
